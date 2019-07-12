9168a26af7233fc613bed414fc34bede487d7119
Vadim Gurkalov <vadim.gurkalov@gmail.com>
2019-07-12 15:49:34 +0300

    completed 1.1

---
+++ osa1/kurssitiedot/src/index.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = (props) => {
+  // palauta kurssin nimi h1-tagilla
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+}
+
+const Content = (props) => {
+  // luo taulukko tehtävien nimistä ja niiden määristä
+  const items = [];
+  for (const key in props.exercises_list) {
+    items.push(<p> {key} {props.exercises_list[key]}</p>);
+  }
+  return (
+    <div>
+      {items}
+    </div>
+  );
+}
+
+
+const Total = (props) => {
+  // poimi tehtävälista-objektista tehtävien määrä per osa ja laske ne yhteen
+  const total = Object.keys(props.exercises_list).map(key => props.exercises_list[key]).reduce((a,b) => a + b,0);
+  // palauta tehtävien määrä
+  return (
+    <div>
+      <p>Number of exercises {total}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // const-määrittelyt
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  // tehtävälista-objekti
+  const exercises_list = {};
+  exercises_list[part1] = exercises1;
+  exercises_list[part2] = exercises2;
+  exercises_list[part3] = exercises3;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content exercises_list={exercises_list} />
+      <Total exercises_list={exercises_list} />
+    </div>
+  )
+}
+
+// const App = () => {
+//   const part1 = 'Fundamentals of React'
+//   const exercises1 = 10
+//   const part2 = 'Using props to pass data'
+//   const exercises2 = 7
+//   const part3 = 'State of a component'
+//   const exercises3 = 14
+//
+//   return (
+//     <div>
+//       <h1>{course}</h1>
+//       <p>
+//         {part1} {exercises1}
+//       </p>
+//       <p>
+//         {part2} {exercises2}
+//       </p>
+//       <p>
+//         {part3} {exercises3}
+//       </p>
+//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//     </div>
+//   )
+// }
+
+ReactDOM.render(<App />, document.getElementById('root'));
