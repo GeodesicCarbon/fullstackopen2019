8cd49ad61d1e0f6d46d8c2aa60552350a14b45c3
Vadim Gurkalov <vadim.gurkalov@gmail.com>
2019-07-25 22:32:45 +0300

    Completed exercise 2.13

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -3,34 +3,67 @@ import axios from 'axios'
 import CountryData from './components/CountryData'
 
 const App = () => {
+  // Tilamuuttujat etsintäkentälle, maille
+  // sekä valitulle maatagille
   const [search, setSearch] = useState('')
   const [countries, setCountries] = useState([])
+  const [countryTag, setCountryTag] = useState('')
 
+  // Haetaan maiden tiedot REST-apin kautta ja tallennetann
+  // ne tilamuuttujaan
   useEffect(() => {
-    console.log('effect')
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        console.log('promise fulfilled')
         setCountries(response.data)
       })
   }, [])
 
-  const countriesToShow = !search
-    ? []
-    : countries.filter(country =>
-        country.name.toLowerCase().includes(
-          search.toLowerCase()
+  // funktio, joka vastaa kaikkien maiden filtteröinnistä
+  const selectCountriesToShow = () => {
+    // jos maatagi-tilamuuttujassa on sisältöä,
+    // näytetään vain se maa
+    if (countryTag)
+      return (
+        countries.filter(country =>
+          country.alpha3Code === countryTag
         )
       )
-
-  const handleSearchChange = (event) => {
-    setSearch(event.target.value)
+    // muutoin muutetaan kaikki nimet pieniin kirjaimiin
+    // ja suodatetaan
+    else
+      return (
+        !search
+          ? []
+          : countries.filter(country =>
+              country.name.toLowerCase().includes(
+                search.toLowerCase()
+              )
+            )
+      )
   }
 
+  // kutsutaan maiden filtteröintifunktiota ja tallenetaan
+  // tulos
+  const countriesToShow = selectCountriesToShow()
+
+  // funktio, joka vastaa etsintäkentä tilamuuttujasta
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
+    setCountryTag('')
+  }
+
+  // funktio, joka vastaa maalistauksen "show"-napin
+  // matagin tallentamisesta tilamuuttujaan
+  const selectCountry = (alpha3Code) => {
+    setCountryTag(alpha3Code)
+  }
+
+  // kootaan etsintäkentälle tarvittavat objektit yhteen
   const searchForm = {
     search,
-    handleSearchChange
+    handleSearchChange,
+    selectCountry
   }
 
   return (
--- osa2/maiden_tiedot/src/components/CountryData.js
+++ osa2/maiden_tiedot/src/components/CountryData.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+// Etsintäkentä komponentti
 const SearchForm = ({searchForm}) => (
   <div>
     Find countries: <input
@@ -9,17 +10,29 @@ const SearchForm = ({searchForm}) => (
   </div>
 )
 
-const ListCountries = ({countries}) => (
+// tekee taulukon hakua vastaavista maiden nimistä ja
+// ja napista, jolla voi maasta nähtä yksityiskohtia
+const ListCountries = ({countries, selectCountry}) => (
     <table>
       <tbody>
-        {countries.map(country => <tr key={country.alpha3Code}><td>{country.name}</td></tr>)}
+        {countries.map(country =>
+          <tr key={country.alpha3Code}>
+            <td>{country.name}</td>
+            <td><button onClick={
+              () => selectCountry(country.alpha3Code)}>
+              show
+            </button></td>
+          </tr>)}
       </tbody>
     </table>
 )
 
+
+// komponentti, joka näyttää maan yksityiskohtia
 const ShowCountry = ({countries}) => {
   const country = countries[0]
 
+  // pienennetään lipun kooksi 30% ruudun koosta
   const imgStyle = {
     width: '30%',
     height: '30%'
@@ -41,29 +54,39 @@ const ShowCountry = ({countries}) => {
   )
 }
 
+// komponentti, joka vastaa maiden tietojen listauksesta
+// sekä nayttämisestä
 const CountryData = ({searchForm, countries}) => {
+  // jos tuloksia ei ole niin näytetän kehote
   if (countries.length === 0) return (
     <div>
       <SearchForm searchForm={searchForm} />
       <div>Use the search form to begin</div>
     </div>
   )
+  // jos tuloksia on yli 10 niin pyydetään tarkentamaan
   if (countries.length > 10) return (
       <div>
         <SearchForm searchForm={searchForm} />
         <div>Too many matches, specify another filter</div>
       </div>
     )
+  // jos tuloksia on 2-10 niin listataan ne
   else if (countries.length === 1) return (
       <div>
         <SearchForm searchForm={searchForm} />
         <ShowCountry countries={countries} />
       </div>
     )
+  // muussa tapauksessa (tuloksia = 1) näytetän
+  // maan tiedot
   else return (
     <div>
       <SearchForm searchForm={searchForm} />
-      <ListCountries countries={countries} />
+      <ListCountries
+        countries={countries}
+        selectCountry={searchForm.selectCountry}
+      />
     </div>
   )
 }
