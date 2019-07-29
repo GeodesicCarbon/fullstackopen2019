dab255b94f2bdb85f2d6d28ff6157c234e4459f4
Vadim Gurkalov <vadim.gurkalov@gmail.com>
2019-07-30 01:19:16 +0300

    Completed exercises 3.5 and 3.6

--- osa3/puhelinluettelobackend/index.js
+++ osa3/puhelinluettelobackend/index.js
@@ -58,6 +58,47 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end()
 })
 
+// Generoidaan ID uudelle henkilölle
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    : 0
+  return maxId + 1
+}
+
+// lisätääm uusi henkilö puhelinluetteloon
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  // tarkistetaan, että sisältö on olemassa
+  if (!body.name) {
+    return res.status(400).json({
+      error: 'name missing'
+    })
+  }
+  if (!body.number) {
+    return res.status(400).json({
+      error: 'number missing'
+    })
+  }
+  // tarkistetaan, että nimi ei ole jo olemassa puhelinluettelossa
+  if (persons.filter(p => p.name === body.name).length) {
+    return res.status(409).json({
+      error: 'name already exists'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+
 // tarjotaan info-sivu
 app.get('/info', (req, res) => {
   const peopleCount = persons.length
