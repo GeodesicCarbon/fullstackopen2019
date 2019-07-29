94c232a2a148ea650fb70b19a1d49e81d0ba7536
Vadim Gurkalov <vadim.gurkalov@gmail.com>
2019-07-29 22:34:46 +0300

    Completed exercise 3.3

--- osa3/puhelinluettelobackend/index.js
+++ osa3/puhelinluettelobackend/index.js
@@ -43,6 +43,14 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+// tarjotaan yhden henkilön tiedot JSON-muodossa
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find( person  => person.id === id)
+  if (person) res.json(person)
+  else res.status(404).end()
+})
+
 // tarjotaan info-sivu
 app.get('/info', (req, res) => {
   const peopleCount = persons.length
