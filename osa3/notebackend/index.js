2a8d3fb3e70ed5b0769017b2006a9a6a15d4e2ea
Vadim Gurkalov <vadim.gurkalov@gmail.com>
2019-08-01 21:31:21 +0300

    Added mongo as a module and implemented dotenv for passwords

--- osa3/notebackend/index.js
+++ osa3/notebackend/index.js
@@ -1,26 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Note = require('./models/note')
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(express.static('build'))
-const mongoose = require('mongoose')
-
-// ÄLÄ KOSKAAN TALLETA SALASANOJA githubiin!
-const url =
-  'mongodb+srv://fullstack:secret@fullstack2019-dzwdj.mongodb.net/note-app?retryWrites=true&w=majority'
-
-mongoose.connect(url, { useNewUrlParser: true })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -57,17 +44,16 @@ app.get('/api', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  Note.find({}).then(notes => {
+    res.json(notes.map(note => note.toJSON()))
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-}})
+  Note.findById(request.params.id).then(note => {
+    response.json(note.toJSON())
+  })
+})
 
 const generateId = () => {
   const maxId = notes.length > 0
@@ -79,22 +65,19 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote.toJSON())
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -110,7 +93,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
---
+++ osa3/notebackend/models/note.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+// ÄLÄ KOSKAAN TALLETA SALASANOJA githubiin!
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url, { useNewUrlParser: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+module.exports = mongoose.model('Note', noteSchema)
