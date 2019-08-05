4ef7bba1e9e9e25b62313340f414fe306ddfa3cb
Vadim Gurkalov <vadim.gurkalov@gmail.com>
2019-08-05 17:02:42 +0300

    Materials part 1

---
+++ osa4/notebackend/models/note.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  date: Date,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
