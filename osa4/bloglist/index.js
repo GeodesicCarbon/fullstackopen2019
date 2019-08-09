abe71e1e64c2d758586a6b6e778452880baa8064
Vadim Gurkalov <vadim.gurkalov@gmail.com>
2019-08-10 02:38:49 +0300

    completed exercise 4.1

---
+++ osa4/bloglist/index.js
@@ -0,0 +1,48 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
+
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  console.log("body: ", request.body)
+  const blog = new Blog(request.body)
+  console.log("mongo object: ", blog)
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
