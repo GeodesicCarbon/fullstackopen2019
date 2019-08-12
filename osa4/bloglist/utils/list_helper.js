3a6fa701853e7acfb8ff14787e66bf5598261858
Vadim Gurkalov <vadim.gurkalov@gmail.com>
2019-08-13 02:52:57 +0300

    completed exercise 4.5 and improved on exercise 4.4

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -1,7 +1,21 @@
-const dummy = (blogs) => {
+const dummy = (blogs) => { // eslint-disable-line no-unused-vars
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce( (sum, blog) => { return sum + blog.likes }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0
+    ? null
+    : blogs.reduce((top, blog) => top.likes >= blog.likes ? top : blog, { likes: -1 })
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
