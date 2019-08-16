e002f6fe0ea2a97027ecb81a79ca91eca215a297
Vadim Gurkalov <vadim.gurkalov@gmail.com>
2019-08-16 17:00:15 +0300

    Created first test for exercise 4.8

--- osa4/bloglist/utils/config.js
+++ osa4/bloglist/utils/config.js
@@ -2,8 +2,13 @@
 require('dotenv').config()
 
 // Määritellään parametrimuuttujat ja palautetaan ne
-const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+let PORT = process.env.PORT
+let MONGODB_URI = process.env.MONGODB_URI
+
+// Määritellään testiympäristölle oma tietokanta
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.TEST_MONGODB_URI
+}
 
 module.exports = {
   MONGODB_URI,
