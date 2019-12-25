e0c94aea9ece25762b9befbecd8bd4d58a930bc8
Vadim Gurkalov <vadim.gurkalov@gmail.com>
2019-12-25 20:12:20 +0200

    Materials part 5.3

--- osa5/notefrontend/src/App.js
+++ osa5/notefrontend/src/App.js
@@ -36,6 +36,15 @@ const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
   }
@@ -46,6 +55,12 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+
+      noteService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
