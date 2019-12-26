d10f56e7095f5591c2d6518216eb407929af02dc
Vadim Gurkalov <vadim.gurkalov@gmail.com>
2019-12-26 22:04:46 +0200

    completed exercise 5.6

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,34 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const showWhenExpanded = { display: expanded ? '' : 'none' }
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded)
+  }
+  if(!blog.user || !blog.user.name)
+    blog.user = {name: 'Uknown'}
+
+  return (
+    <div className="blog">
+      <div onClick={() => toggleExpanded()}>
+        {blog.title} {blog.author}
+      </div>
+      <div style ={showWhenExpanded}>
+        <div>
+          <a href={blog.url}>{blog.url}</a>
+        </div>
+        <div>
+          {blog.likes} likes <button>Like</button>
+        </div>
+        <div>
+          Added by {blog.user.name}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
