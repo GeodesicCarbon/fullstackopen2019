c06ba8846a5e36b4399ce0547cedcb6768b60196
Vadim Gurkalov <vadim.gurkalov@gmail.com>
2019-12-30 16:06:25 +0200

    completed exercises 6.1 and 6.2

--- osa6/unicafe-redux/src/reducer.test.js
+++ osa6/unicafe-redux/src/reducer.test.js
@@ -32,4 +32,116 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('good is incremented twice', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    deepFreeze(newState)
+    const twiceState = counterReducer(newState, action)
+    expect(twiceState).toEqual({
+      good: 2,
+      ok: 0,
+      bad: 0
+    })
+  })
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('ok is incremented twice', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    deepFreeze(newState)
+    const twiceState = counterReducer(newState, action)
+    expect(twiceState).toEqual({
+      good: 0,
+      ok: 2,
+      bad: 0
+    })
+  })
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('bad is incremented twice', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    deepFreeze(newState)
+    const twiceState = counterReducer(newState, action)
+    expect(twiceState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 2
+    })
+  })
+  test('zero resets the state', () => {
+    const action1 = {
+      type: 'GOOD'
+    }
+    const action2 = {
+      type: 'OK'
+    }
+    const action3 = {
+      type: 'BAD'
+    }
+    const action4 = {
+      type: 'BAD'
+    }
+    const reset = {
+      type: 'ZERO'
+    }
+    let state = counterReducer(initialState, action1)
+    state = counterReducer(state, action2)
+    state = counterReducer(state, action3)
+    state = counterReducer(state, action4)
+    deepFreeze(state)
+    expect(state).toEqual({
+      good: 1,
+      ok: 1,
+      bad: 2
+    })
+    const newState = counterReducer(state, reset)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
 })
