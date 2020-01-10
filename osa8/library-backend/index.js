79a663333f3f55dedc60ba4daab27e6290cfba69
Vadim Gurkalov <vadim.gurkalov@aalto.fi>
2020-01-10 10:40:40 +0200

    completed exercise 8.3

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -93,11 +93,19 @@ const typeDefs = gql`
     id: ID!
   }
 
+  type Author {
+    name: String!
+    born: Int
+    id: ID!
+    bookCount: Int
+  }
+
   type Query {
     hello: String!
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -106,7 +114,11 @@ const resolvers = {
     hello: () => { return "world" },
     bookCount: () => { return books.length },
     authorCount: () => { return authors.length },
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => books.filter(b => b.author === root.name).length
   }
 }
 
