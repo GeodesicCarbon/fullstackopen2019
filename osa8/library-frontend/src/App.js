65bda369fb3718e08c4a4a8e5fef11497380c261
Vadim Gurkalov <vadim.gurkalov@aalto.fi>
2020-01-10 11:49:36 +0200

    completed exercise 8.8

---
+++ osa8/library-frontend/src/App.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import { gql } from 'apollo-boost'
+import { useQuery, useMutation } from '@apollo/react-hooks'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+
+const ALL_AUTHORS = gql`
+  {
+    allAuthors {
+      name
+      born
+      id
+      bookCount
+    }
+  }
+`
+
+const ALL_BOOKS = gql`
+ {
+   allBooks {
+     title
+     author
+     id
+     published
+   }
+ }
+`
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+
+  const authors = useQuery(ALL_AUTHORS)
+  const books = useQuery(ALL_BOOKS)
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        show={page === 'authors'}
+        result={authors}
+      />
+
+      <Books
+        show={page === 'books'}
+        result={books}
+      />
+
+      <NewBook
+        show={page === 'add'}
+      />
+
+    </div>
+  )
+}
+
+export default App
---
+++ osa8/library-frontend/src/components/Authors.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { gql } from 'apollo-boost'
+import { useApolloClient } from '@apollo/react-hooks'
+
+const Authors = ({ show, result }) => {
+
+  if (!show) {
+    return null
+  }
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {result.data.allAuthors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
